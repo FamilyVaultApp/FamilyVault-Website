ab24e822519aba4368303b9213274a76dc2bd809
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2024-12-04 23:13:41 +0100

    Create WelcomeAnimation

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import Footer from "./components/Footer";
 import AboutProject from "./components/InfoBoxes/AboutProject";
 import Meetings from "./components/InfoBoxes/Meetings";
 import TeamMembers from "./components/InfoBoxes/TeamMembers";
 import Timetable from "./components/InfoBoxes/Timetable";
-import TopBar from "./components/TopBar";
 import "bootstrap/dist/css/bootstrap.css";
+import WelcomeAnimation from "./components/WelcomeAnimation/WelcomeAnimation.tsx";
 
 function App() {
-  return (
-    <>
-      <TopBar />
-      <AboutProject />
-      <TeamMembers />
-      <Meetings />
-      <Timetable />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <WelcomeAnimation/>
+            <AboutProject/>
+            <TeamMembers/>
+            <Meetings/>
+            <Timetable/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/WelcomeAnimation/WelcomeAnimationAppName.tsx
@@ -0,0 +1,11 @@
+import "./WelcomeAnimationAppName.scss";
+
+const WelcomeAnimationAppName = () => {
+    return <div className="welcome-animation__app-name">
+        <h1>
+            Family Vault
+        </h1>
+    </div>
+}
+
+export default WelcomeAnimationAppName;

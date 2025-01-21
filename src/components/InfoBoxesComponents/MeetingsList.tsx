b576b002fa18d233d7562b5ccdb41751cf8430ac
Bartłomiej Wiśniewski <148913398+bwisniewski26@users.noreply.github.com>
2025-01-21 22:53:10 +0100

    Update app name and meetings list

--- src/components/InfoBoxesComponents/MeetingsList.tsx
+++ src/components/InfoBoxesComponents/MeetingsList.tsx
@@ -1,36 +1,52 @@
 import MeetingEntry from "./MeetingEntry.tsx";
-import "./MeetingsList.scss"
+import "./MeetingsList.scss";
 
 const MeetingsList = () => {
   return (
-      <>
-        <div className="meetings-list">
-          <MeetingEntry
-              title="Spotkanie organizacyjne"
-              date="6 listopada 2024"
-              agendaItems={[
-                "Zapoznanie zespołu z opiekunem",
-                "Przedstawienie wstępnych propozycji tematu projektu"
-              ]}
-          />
-          <MeetingEntry
-              title="Rozważania nad tematem projektu"
-              date="13 listopada 2024"
-              agendaItems={[
-                "Analiza pomysłów na projekt",
-                "Rozważania nad rozbudową potencjalnych wyborów"
-              ]}
-          />
-          <MeetingEntry
-              title="Ostateczny wybór tematu projektu"
-              date="27 listopada 2024"
-              agendaItems={[
-                "Rozważenie dodatkowych funkcjonalności",
-                "Omówienie potencjalnych technologii, które można wykorzystać"
-              ]}
-          />
-        </div>
-      </>
+    <>
+      <div className="meetings-list">
+        <MeetingEntry
+          title="Spotkanie organizacyjne"
+          date="6 listopada 2024"
+          agendaItems={[
+            "Zapoznanie zespołu z opiekunem",
+            "Przedstawienie wstępnych propozycji tematu projektu",
+          ]}
+        />
+        <MeetingEntry
+          title="Rozważania nad tematem projektu"
+          date="13 listopada 2024"
+          agendaItems={[
+            "Analiza pomysłów na projekt",
+            "Rozważania nad rozbudową potencjalnych wyborów",
+          ]}
+        />
+        <MeetingEntry
+          title="Ostateczny wybór tematu projektu"
+          date="27 listopada 2024"
+          agendaItems={[
+            "Rozważenie dodatkowych funkcjonalności",
+            "Omówienie potencjalnych technologii, które można wykorzystać",
+          ]}
+        />
+        <MeetingEntry
+          title="Podział obowiązków"
+          date="11 grudnia 2024"
+          agendaItems={[
+            "Podział obowiązków w zespole",
+            "Przygotowanie wstępnego projektu UI",
+          ]}
+        />
+        <MeetingEntry
+          title="Omówienie postępów"
+          date="8 stycznia 2025"
+          agendaItems={[
+            "Omówienie postępów w pracy",
+            "Początek pracy nad user flow",
+          ]}
+        />
+      </div>
+    </>
   );
 };
 

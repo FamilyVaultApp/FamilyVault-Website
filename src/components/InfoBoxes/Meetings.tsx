fe08eae1fa1d42765036d2c877df7fc4b995f323
Bartłomiej Wiśniewski <148913398+bwisniewski26@users.noreply.github.com>
2025-03-25 12:52:00 +0100

    Update information on the website, add team photo

--- src/components/InfoBoxes/Meetings.tsx
+++ src/components/InfoBoxes/Meetings.tsx
@@ -10,10 +10,8 @@ const Meetings = () => {
           <InfoBoxHeader>Spotkania zespołu</InfoBoxHeader>
           <br />
           <div className="infobox-content">
-            Spotkania zespołu odbywają się w środy w trybie dwutygodniowym w
+            Spotkania zespołu odbywają się w poniedziałki w trybie dwutygodniowym w
             siedzibie firmy <b>Simplito</b> - opiekuna projektu. <br />
-            Dodatkowo w każdy poniedziałek zespół odbywa z opiekunem zdalną
-            rozmowę, w celu omówienia bieżących problemów oraz pomysłów.
             <br />
           </div>
           <br />

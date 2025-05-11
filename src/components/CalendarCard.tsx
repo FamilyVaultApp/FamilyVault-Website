30c4dab0fe97564bfd88de60cbf3aa9107dd54e7
Bartłomiej Wiśniewski <148913398+bwisniewski26@users.noreply.github.com>
2025-05-11 13:44:23 +0200

    Fix errors during npm build

--- src/components/CalendarCard.tsx
+++ src/components/CalendarCard.tsx
@@ -2,19 +2,19 @@ import { ReactNode } from "react";
 import "./CalendarCard.scss";
 
 interface CalendarCardProps {
-    month: string,
-    day: string,
-    description: ReactNode
+    month: string;
+    day: number;
+    description: ReactNode;
 }
 
 const CalendarCard = ({ month, day, description }: CalendarCardProps) => {
-    return <div className="calendar-card">
-        <p className="calendar-card-month">{month}</p>
-        <p className="calendar-card-day">{day}</p>
-        <div className="calendar-card-description">
-            {description}
+    return (
+        <div className="calendar-card">
+            <p className="calendar-card-month">{month}</p>
+            <p className="calendar-card-day">{day}</p>
+            <div className="calendar-card-description">{description}</div>
         </div>
-    </div>;
+    );
 };
 
 export default CalendarCard;

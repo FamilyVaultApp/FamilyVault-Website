a2febc24c6f3fdf1a425871ea3f6fddd3c0bf16d
Bartłomiej Wiśniewski <148913398+bwisniewski26@users.noreply.github.com>
2025-05-21 18:08:14 +0200

    Fix formatting

--- src/components/sections/MottoSection.tsx
+++ src/components/sections/MottoSection.tsx
@@ -7,20 +7,14 @@ const MottoSection = () => {
         <Section>
             <div className="motto-section">
                 <h1 className="motto-section">
-                    Ponieważ <b>każda rodzina</b> zasługuje na{" "}
-                    <AnimatedHighlightedText>
-                        prywatny i bezpieczny
-                    </AnimatedHighlightedText>{" "}
-                    komunikator.
+                    Ponieważ <b>każda rodzina</b> zasługuje na <AnimatedHighlightedText>prywatny i bezpieczny</AnimatedHighlightedText>komunikator.
                 </h1>
                 <div className="motto-about">
                     <p>
-                        FamilyVault wspiera codzienną komunikację,{" "}
-                        <b>chroniąc Wasze rozmowy</b> i <b>rodzinne sprawy</b>.
-                        Ta <b>bezpieczna alternatywa</b> do popularnych
-                        komunikatorów zawiera tylko niezbędne funkcje, jak czat,
-                        wiadomości głosowe, wysyłanie zdjęć i lista zadań –{" "}
-                        <b>bez rozpraszania, śledzenia i reklam</b>.
+                        FamilyVault wspiera codzienną komunikację, <b>chroniąc Wasze rozmowy</b> i <b>rodzinne
+                        sprawy</b>. Ta <b>bezpieczna alternatywa</b> do popularnych komunikatorów zawiera tylko
+                        niezbędne funkcje, jak czat, wiadomości głosowe, wysyłanie zdjęć i lista zadań - <b>bez
+                        rozpraszania, śledzenia i reklam</b>.
                     </p>
                     <div className="motto-about--elements">
                         <ul>
